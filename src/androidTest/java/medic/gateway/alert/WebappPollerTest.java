f336d8e782bdf04c5987490a94170349144e1c56
Estelle Comment <estelle@medicmobile.org>
2017-04-28 14:53:37 +0200

    Extra test, to check what the polling sends to api

--- src/androidTest/java/medic/gateway/alert/WebappPollerTest.java
+++ src/androidTest/java/medic/gateway/alert/WebappPollerTest.java
@@ -90,12 +90,46 @@ public class WebappPollerTest extends AndroidTestCase {
 		// then
 		JSONObject requestBody = http.assertPostRequestMade_withJsonResponse();
 
-		JSONArray messages = requestBody.getJSONArray("updates");
-		assertEquals(1, messages.length());
+		JSONArray updates = requestBody.getJSONArray("updates");
+		assertEquals(1, updates.length());
 
-		JSONObject message = (JSONObject) messages.get(0);
-		assertEquals(deliveredId, message.getString("id"));
-		assertEquals("DELIVERED", message.getString("status"));
+		JSONObject update = (JSONObject) updates.get(0);
+		assertEquals(deliveredId, update.getString("id"));
+		assertEquals("DELIVERED", update.getString("status"));
+	}
+
+	@Test
+	public void test_pollWebapp_shouldIncludeMultipleStatusUpdatesThatNeedForwardingForTheSameMessage()
+			throws Exception {
+		// given : one message...
+		String messageId = randomUuid();
+		db.insert("wo_message",
+				cols("_id",      "status",                   "last_action", "_to",         "content"),
+				vals(messageId,  WoMessage.Status.DELIVERED, 0,             A_PHONE_NUMBER, SOME_CONTENT));
+		// ... with 3 successive statuses, one of which was already forwarded to api.
+		db.insert("wom_status",
+				cols("message_id", "status",                "timestamp", "needs_forwarding"),
+				vals(messageId,  WoMessage.Status.PENDING,  0,            false),
+				vals(messageId,  WoMessage.Status.SENT,     0,            true),
+				vals(messageId, WoMessage.Status.DELIVERED, 0,            true));
+		http.nextResponseJson("{}");
+
+		// when
+		new WebappPoller(getContext()).pollWebapp();
+
+		// then : the two non-forwarded status were forwarded.
+		JSONObject requestBody = http.assertPostRequestMade_withJsonResponse();
+
+		JSONArray updates = requestBody.getJSONArray("updates");
+		assertEquals(2, updates.length());
+
+		JSONObject update1 = (JSONObject) updates.get(0);
+		assertEquals(messageId, update1.getString("id"));
+		assertEquals("SENT", update1.getString("status"));
+
+		JSONObject update2 = (JSONObject) updates.get(0);
+		assertEquals(messageId, update2.getString("id"));
+		assertEquals("SENT", update2.getString("status"));
 	}
 
 	@Test
